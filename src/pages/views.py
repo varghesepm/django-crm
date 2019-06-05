dd4dd2bb90bc04938282cc5a24892b0720d0f753
varghese <varghesepoyyali@gmail.com>
2019-06-05 23:04:03 +0530

    changes

--- src/pages/views.py
+++ src/pages/views.py
@@ -9,7 +9,11 @@ def home_main(request,*args,**kwargs):
 	return render(request, "home.html", {})
 
 def contact_main(*args,**kwargs):
-	return HttpResponse("<h1>Contact Page</h>")
-	
-def about_main(*args,**kwargs):
-	return HttpResponse("<h1>About Page</h>")
+	return render(request, "contact.html", {})
+
+def about_main(request,*args,**kwargs):
+	my_context = {
+			"my_tittle": "About Us",
+			"my_number": 123456
+	}
+	return render(request, "about.html", my_context )
